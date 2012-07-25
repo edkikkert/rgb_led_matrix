4695d36bbf2f57cbf093b35b773c95faacd4991a
KarloLuiten <github@karloluiten.nl>
2012-07-25 22:55:59 +0200

    Instructions for user

--- server/index.php
+++ server/index.php
@@ -12,9 +12,18 @@ $con=json_decode(file_get_contents(realpath('./banner.txt')),true);
 <style>
 input{font-family:monospace;}
 </style>
+<pre>
+  'R'       Red
+  'G'       Green
+  'B'       BLue
+  'Y'       Yellow
+  'V'       Violet
+  'T'       Teal/turquise
+  'Z'       Off
+</pre>
 <form method="post">
 <input name="data" value="<?=$con['data']?>"/>&lt; text<br/>
-<input name="colors" value="<?=$con['colors']?>"/>&lt; colors (r/g/b)<br/>
+<input name="colors" value="<?=$con['colors']?>"/>&lt; color<br/>
 <input name="time" value="<?=$con['time']?>"/>&lt; time between steps (secs)<br/>
     <input type="submit"/>
 </form>
