3c6278e1ffc2fe65ce7d64505b9a3e53d33094a4
karlo <karlo@leanback.eu>
2012-07-25 16:10:02 +0200

    Server files

--- server/index.php
+++ server/index.php
@@ -1,3 +1,9 @@
 <?php
-phpinfo();
+if(isset($_POST['data'])){
+    file_put_contents(realpath('./banner.txt'),$_POST['data']);
+}
 ?>
+<form method="post">
+<input name="data" value="<?php echo file_get_contents(realpath('./banner.txt')) ?>"/>
+    <input type="submit"/>
+</form>
