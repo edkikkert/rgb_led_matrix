83e32cfc40a364c4ed6b76b94c7cb8788f87f268
karlo <karlo@leanback.eu>
2012-07-25 17:07:13 +0200

    Server side files for editing test

--- server/index.php
+++ server/index.php
@@ -1,9 +1,20 @@
 <?php
 if(isset($_POST['data'])){
-    file_put_contents(realpath('./banner.txt'),$_POST['data']);
+    $d=$_POST['data'];
+    $c=$_POST['colors'];
+    $t=$_POST['time'];
+    $data='{"data":"'.$d.'","colors":"'.$c.'","time":'.$t.'}';
+    file_put_contents(realpath('./banner.txt'),$data);
 }
+
+$con=json_decode(file_get_contents(realpath('./banner.txt')),true);
 ?>
+<style>
+input{font-family:monospace;}
+</style>
 <form method="post">
-<input name="data" value="<?php echo file_get_contents(realpath('./banner.txt')) ?>"/>
+<input name="data" value="<?=$con['data']?>"/>&lt; text<br/>
+<input name="colors" value="<?=$con['colors']?>"/>&lt; colors (r/g/b)<br/>
+<input name="time" value="<?=$con['time']?>"/>&lt; time between steps (secs)<br/>
     <input type="submit"/>
 </form>
